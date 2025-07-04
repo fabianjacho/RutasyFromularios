d4496a4f3ddd7964898837e8239984993e7d2ba9
Fabian <fabianjacho@gmail.com>
2025-07-04 18:40:43 -0500

    ejercicio de rutas y formularios

--- src/app/formulario-normal/formulario-normal.component.ts
+++ src/app/formulario-normal/formulario-normal.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-normal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario-normal.component.html',
   styleUrl: './formulario-normal.component.scss'
 })
 export class FormularioNormalComponent {
+  nombre: string = '';
+  correo: string = '';
 
+  
 }
